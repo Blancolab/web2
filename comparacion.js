17de0b116ed5d79e8ccf6b872ef843c9fcab9272
Blancolab <mozartmarlone@icloud.com>
2025-09-03 20:46:40 -0600

    Add files via upload

---
+++ comparacion.js
@@ -0,0 +1,17 @@
+
+var numero =0;
+if(numero){
+    console.log("TRUE");
+}else{
+    console.log("False");
+}
+
+var numero = 1;
+
+while ( numero < 100){
+console.log("El valor es: "+numero);
+// numero ++;
+numero = numero + 1
+}
+
+
